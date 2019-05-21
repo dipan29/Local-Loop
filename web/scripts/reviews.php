7816befed32780a81de8546206a2ea2a77799f24
Dipan Roy <dipan29@users.noreply.github.com>
2019-05-21 23:03:47 +0530

    Update reviews.php

--- web/scripts/reviews.php
+++ web/scripts/reviews.php
@@ -66,6 +66,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Reviews API - Local Loop</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
